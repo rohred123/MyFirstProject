74267d88b766e078ac1ce124e063ba9fb7aa0f8c
rohred123 <vigyana@yahoo.com>
2020-05-11 01:53:37 +0000

    Wrote some basic code

---
+++ main.py
@@ -0,0 +1,23 @@
+print (19283409823094823094823098409238049328094830298409832098423098/934850934850934805648309584390840958349058340985904)
+print (234324*243242342934792817491739712839471938)
+print (10+10)
+print (86-22)
+print ("Rohan")
+print ("Rohan is very nice" )
+print ("10+10 =" + "15")
+print ("10+10 =" + str(10) + str(10) ) 
+print ("10+10 = " + str(10 + 10))
+print ("I teach Python" + " using repl.it")
+print ("45*45 = "+ str (45*45))
+print (8242874+92840329)
+print ("12391+923409 = " + str(12391+923409))
+# This is a comment. How do you do this.
+print ("today is mothers day")
+print ( """Hello World""")
+print ( '"Hello World"')
+print ('''Hello World
+
+hi 
+
+
+new line ''')
