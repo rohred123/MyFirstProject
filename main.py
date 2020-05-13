ada88a98246f7374bdd12d3f30050473ab65d1ac
rohred123 <vigyana@yahoo.com>
2020-05-13 19:33:50 +0000

    I added some new things I learned now.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print (19283409823094823094823098409238049328094830298409832098423098/934850934850934805648309584390840958349058340985904)
 print (234324*243242342934792817491739712839471938)
 print (10+10)
-print (86-22)
+print (86-22) 
 print ("Rohan")
 print ("Rohan is very nice" )
 print ("10+10 =" + "15")
@@ -23,10 +23,45 @@ hi
 new line ''')
 print(234234+9242)
 print(23-234234)
-print( '''Hello World 
+print( '''Hello World ''')
+print( 92340928409238+98092384099)
+print("hello people")
+print("12+234 = " + str(12+234))
+if 5 > 2:
+ print("Five is greater than two!")  
+if 5 > 2:
+        print("Five is greater than two!") 
+x=7
+y=(5)
+
+print(x)
+print(y)
+print(x+y)
+
+"""
 hi
 
+this is Joe"""
+
+("""
 hi
+this is Joe""")
+
+('''Hello World
+
+
+hi           ''')
+print ('''Hello World
+
+hi 
+
+
+new line ''')
+print ('''Hello World
+
 hi
 
-what is this''')
+
+new line''') 
+wj
+
