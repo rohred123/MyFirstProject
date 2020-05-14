3d1d72d79efa0a4d230fa1742bcc49cb85c52d53
rohred123 <vigyana@yahoo.com>
2020-05-14 02:52:07 +0000

    some stuff

--- main.py
+++ main.py
@@ -82,5 +82,19 @@ cupcakes_made=520
 cupcakes_spoilt=12
 cost_of_each_cupcake=25
 print((cupcakes_made - cupcakes_spoilt)* cost_of_each_cupcake)
+print("veggie sticks are yummy")
 
-
+print('This is Rohan\'s computer')
+age=123123
+display_text="Sophia is %s years old"
+print(display_text %age)
+age=11
+after_five_years=age+5
+after_ten_years=age+10
+print("Rohan will be %s years old after five years" % after_five_years)
+print("Rohan will be %s years old after ten years" % after_ten_years)
+name="Diya"
+age=1
+display_text="%s is %s years old" 
+print(display_text % (name,age))
+print("After five years, %s will be %s years old"% (name,age+5))
