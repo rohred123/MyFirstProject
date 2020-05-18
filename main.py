1544a6ec50a11d1c9fd385b2e53252c075bf0e62
rohred123 <vigyana@yahoo.com>
2020-05-18 03:49:34 +0000

    nothing

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-
-
 first_name= input('Enter Your First Name: ')
 last_name= input('Enter Your Last Name: ')
 address = input ('Enter your street address:')
@@ -11,3 +8,7 @@ print("Name : " + first_name + ' ' + last_name)
 print ("Address : " + address)
 print ('Zip : ' + zipcode)
 print ('age: ' + str(age))
+x=2**237678687687687652%234
+print(x)
+y=2342432344234234232433333333333333333333333332342347294209840293840923840928402389408230948209384029840923840928093489284280428094820840238490234902839482903840293%126576576565998798789678858758
+print(y)
