462d79dc578e676938330bc54566e9cd3403acc5
rohred123 <vigyana@yahoo.com>
2020-05-18 01:43:12 +0000

    I deleted my codes and added some

--- main.py
+++ main.py
@@ -1,124 +1,13 @@
-print (19283409823094823094823098409238049328094830298409832098423098/934850934850934805648309584390840958349058340985904)
-print (234324*243242342934792817491739712839471938)
-print (10+10)
-print (86-22) 
-print ("Rohan")
-print ("Rohan is very nice" )
-print ("10+10 =" + "15")
-print ("10+10 =" + str(10) + str(10) ) 
-print ("10+10 = " + str(10 + 10))
-print ("I learn Python" + " using repl.it")
-print ("45*45 = "+ str (45*45))
-print (8242874+92840329)
-print ("12391+923409 = " + str(12391+923409))
-# This is a comment. How do you do this.
-print ("today is mothers day")
-print ( """Hello World""")
-print ( '"Hello World"')
-print ('''Hello World
-
-hi 
 
 
-new line ''')
-print(234234+9242)
-print(23-234234)
-print( '''Hello World ''')
-print( 92340928409238+98092384099)
-print("hello people")
-print("12+234 = " + str(12+234))
-if 5 > 2:
- print("Five is greater than two!")  
-if 5 > 2:
-        print("Five is greater than two!") 
-x=7
-y=(5)
 
-print(x)
-print(y)
-print(x+y)
-
-"""
-hi
-
-this is Joe"""
-
-("""
-hi
-this is Joe""")
-
-('''Hello World
-
-
-hi           ''')
-print ('''Hello World
-
-hi 
-
-
-new line ''')
-print ('''Hello World
-
-hi
-
-
-new line''') 
-
-x, y, z = "Orange", "Banana", "Cherry"
-print(x)
-print(y)
-x = y = z = "Orange"
-print(x)
-print(y)
-print(z)
-
-x = "awesome"
-print("Python is " + x)
-x="okay"
-print( "diya is " + x)
-print("hello world")
-print("123+123")
-cupcakes_made=520
-cupcakes_spoilt=12
-cost_of_each_cupcake=25
-print((cupcakes_made - cupcakes_spoilt)* cost_of_each_cupcake)
-print("veggie sticks are yummy")
-
-print('This is Rohan\'s computer')
-age=123123
-display_text="Sophia is %s years old"
-print(display_text %age)
-age=11
-after_five_years=age+5
-after_ten_years=age+10
-print("Rohan will be %s years old after five years" % after_five_years)
-print("Rohan will be %s years old after ten years" % after_ten_years)
-name="Diya"
-age=1
-display_text="%s is %s years old" 
-print(display_text % (name,age))
-print("After five years, %s will be %s years old"% (name,age+5))
-name = input('Enter your name : ')
-print ('Your name is ' + name)
 first_name= input('Enter Your First Name: ')
 last_name= input('Enter Your Last Name: ')
-print('Your first name is, ' + first_name + ' and your last name is ' + last_name)
-Rohan= input("Please enter a very good word in this, preferably a word you would call your very best friend:")
-print('Rohan is ' + Rohan + '.')
-
-var1 = 89
-print ( type(var1))
-
-var2 ='rohan'
-print ( type(var2))
-Rohan_bulb=False
-print (type(Rohan_bulb))
-x="7"
-y="4.4"
-print( int(x) + float(y))
-print(x+y)
-a = input("Enter a value for a: ")
-b = input("Enter a value for b: ")
-a=int(a)
-b=int(b)
-print(f"The sum of {a} and {b} is " + str (a+b) + ".") 
+address = input ('Enter your street address:')
+zipcode = input('Enter your zip code:')
+birth_year = int(input ('Enter your birth year:'))
+age=2020- birth_year
+print("Name : " + first_name + ' ' + last_name)
+print ("Address : " + address)
+print ('Zip : ' + zipcode)
+print ('age: ' + str(age))
