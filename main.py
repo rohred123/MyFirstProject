74d09d1cad486e1157051fe5a9e0e62fa5a16707
null null <vigyana@yahoo.com>
2020-05-25 00:21:14 +0000

    nothing

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-first_name= input('Enter Your First Name: ')
-last_name= input('Enter Your Last Name: ')
-address = input ('Enter your street address:')
-zipcode = input('Enter your zip code:')
-birth_year = int(input ('Enter your birth year:'))
-age=2020- birth_year
-print("Name : " + first_name + ' ' + last_name)
-print ("Address : " + address)
-print ('Zip : ' + zipcode)
-print ('age: ' + str(age))
-x=2**237678687687687652%234
-print(x)
-y=2342432344234234232433333333333333333333333332342347294209840293840923840928402389408230948209384029840923840928093489284280428094820840238490234902839482903840293%126576576565998798789678858758
-print(y)
+input_string = input('Enter a number: ');
+#print (input_string);
+
+input_string = str(input_string)
+if(input_string.isdigit()):
+  #print ('input is digit')
+  in_int = int(input_string);
+  if(in_int%2 == 0):
+    print('Output is '+ str(in_int+3))
+  else:
+    print('Output is ' + str(in_int*4))
+else:
+  print ('input is not digit. Try again')
+
+print(round(5.5))  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
