ba0d4fb15e6b0998fdf422a31460550920acb2fb
rohred123 <vigyana@yahoo.com>
2020-05-31 18:48:31 +0000

    kg pound converter and more

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-print("This is a kg to lb converter")
-x= input("Please enter your weight in either kgs or lbs and then click enter to proceed: ")
-y= input("Enter the units you used in the first question. Please put in the form kgs or lbs. Then click enter: ")
+print("This is a kg to lb converter.")
+x= input("Please enter your weight : ")
+y= input("Enter the units of the weight you put before this. In the format lbs or kgs: ")
 x= int(x);
-if y=="kgs":
+y= y.casefold();
+if y=="kgs" or y=="kg":
   print("Weight in pounds is  " + str(x*2.205))
-elif y== 'lbs':
+elif y== 'lbs' or y=="lb":
   print("Weight in kg is " + str(x/2.205))
 else:
   print ("You can't follow directions.")
+
+name = 'Rohan';
+print('Name is ' + name[-2]);
+print(len(name))
+
+from scipy.special import gammaln
+from math import log, floor
+
+def digits_in_factorial(n):
+    return floor( gammaln(n+1)/log(10.0) ) + 1
+
+var=digits_in_factorial(25)
+print(var)
+
+function factorial(n) 
+    // Put your code here.
+    if (n<0){
+     console.log("no negatives!");
+    }
+    if(n === 0){
+      return 1;
+  }
+    return n*factorial(n-1);
+}
+factorial(100);
+
+
+
+
+
 
 
 
